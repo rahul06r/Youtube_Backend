ef54e350f30926b8b46315c31f7bcd8df8dc8141
rahul06r <lzrahul58@gmail.com>
2024-04-29 14:07:15 +0530

    Usera & Video Model ,JWT & Bcrypt

--- Readme.md
+++ Readme.md
@@ -7,3 +7,10 @@ https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj
 
 # gitignore generator 
 https://mrkandreev.name/snippets/gitignore-generator/
+
+
+
+# JWT TOKEN GENERATOR
+
+
+https://jwt.io/
---
+++ src/models/video.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+
+
+import mongooseAggregatePaginate  from "mongoose-aggregate-paginate-v2";
+
+
+
+const videoSchmea = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudnary URl
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudnary URl
+            required: true
+
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required:true
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type:Boolean,
+            deafult:true
+        },
+        owner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+
+
+    },
+    { timestamps: true, })
+
+
+
+
+    videoSchmea.plugin(mongooseAggregatePaginate)
+
+
+
+
+export const Video = mongoose.model("Video", videoSchmea)
