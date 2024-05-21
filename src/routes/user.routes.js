0fa0ab01a1afd26402384171ce2139bed5bfa00a
rahul06r <lzrahul58@gmail.com>
2024-05-21 12:43:06 +0530

    User Routes Updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+    changePassword,
+    getUserChannelProfile,
+    getUserWatchHistroy,
+    getcurrentUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDeatils,
+    userAvatarUpdate,
+    userCoverUpdate
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middlware.js";
 
@@ -32,6 +44,30 @@ router.route("/logout").post(verifyJWT, logoutUser)
 
 // 
 router.route("/refresh-token").post(refreshAccessToken)
+
+
+// 
+router.route("/change-password").post(verifyJWT, changePassword)
+// 
+router.route("/current-user").get(verifyJWT, getcurrentUser)
+
+// 
+router.route("/update-account").patch(verifyJWT, updateAccountDeatils)
+
+// 
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), userAvatarUpdate)
+
+// 
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), userCoverUpdate)
+
+// 
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+// 
+router.route("/history").get(verifyJWT, getUserWatchHistroy)
+
+// 
+
 
 
 
