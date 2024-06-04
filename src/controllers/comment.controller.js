0500ad9e7e59ff20b8954a7ef9c12b3191e35f34
rahul06r <lzrahul58@gmail.com>
2024-06-04 17:24:42 +0530

    Fixed and Added Comment to Community Post

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,6 +5,7 @@ import { asyncHandler } from "../utils/asynchandler.js"
 import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js"
 import { Video } from "../models/video.model.js";
+import { CommunityPost } from "../models/communityPost.model.js";
 
 
 
@@ -121,14 +122,7 @@ const deleteCommentVideo = asyncHandler(async (req, res) => {
 })
 // 
 const getAllVideosComment = asyncHandler(async (req, res) => {
-    const { videoId } = req.params;
-    const { page, limit, sort } = req.query;
-    const options = {
-        page: parseInt(page) || 1,
-        limit: parseInt(limit) || 10,
-        sort: { createdAt: parseInt(sort) || 1 }
-    }
-
+    const { page, limit, videoId, sort } = req.query;
     try {
 
         if (!req.user?._id && !isValidObjectId(req.user?._id)) {
@@ -137,62 +131,88 @@ const getAllVideosComment = asyncHandler(async (req, res) => {
         if (!videoId && !isValidObjectId(videoId)) {
             throw new ApiError(404, "No video id found!!");
         }
-        const allComments = await Comment.aggregate(
-            [
-                {
-                    $match: {
-                        video: new mongoose.Types.ObjectId(videoId),
-                    }
-                },
-                {
-                    $lookup: {
-                        from: "users",
-                        localField: "commentedBy",
-                        foreignField: "_id",
-                        as: "userInfo",
-                        pipeline: [
-                            {
-                                $project: {
-                                    username: 1,
-                                    avatar: 1
-                                }
-                            }
-                        ]
-                    }
-                },
+        console.log("Video Id", videoId);
+        
 
-                {
-                    $addFields: {
-                        userInfo: {
-                            $first: "$userInfo"
+
+        const commentsAggregate =  Comment.aggregate([
+            {
+                $match: {
+                    video: new mongoose.Types.ObjectId(videoId),
+                },
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "commentedBy",
+                    foreignField: "_id",
+                    as: "owner",
+                },
+            },
+            {
+                $lookup: {
+                    from: "likes",
+                    localField: "_id",
+                    foreignField: "commentedBy",
+                    as: "likes",
+                },
+            },
+            {
+                $addFields: {
+                    likesCount: {
+                        $size: "$likes",
+                    },
+                    owner: {
+                        $first: "$owner",
+                    },
+                    isLiked: {
+                        $cond: {
+                            if: { $in: [req.user?._id, "$likes.likedBy"] },
+                            then: true,
+                            else: false
                         }
                     }
                 },
-                {
-                    $sort: {
-                        createdAt: parseInt(sort) || 1
-                    }
+            },
+            {
+                $project: {
+                    content: 1,
+                    createdAt: 1,
+                    updatedAt: 1,
+                    likesCount: 1,
+                    owner: {
+                        username: 1,
+                        fullName: 1,
+                        avatar: 1,
+                    },
+                    isLiked: 1
                 },
-                {
-                    $limit: parseInt(limit)
-                },
-                {
-                    $skip: (parseInt(page) - 1) * parseInt(limit)
-                },
-                {
-                    $unwind: "$userInfo"
-                },
-            ])
-        if (!allComments) {
+            },
+        ]);
+
+        if (!commentsAggregate) {
             throw new ApiError(500, "Something went wrong!!");
         }
-        // const result = await Comment.aggregatePaginate(
-        //     allComments,
-        //     options
-        // )
+        console.log("All comments", commentsAggregate.length);
+        const options = {
+            page: parseInt(page, 10),
+            limit: parseInt(limit, 10),
+            sort: ({
+                createdAt: parseInt(sort) || 1
+            })
+        };
+        const result = await Comment.aggregatePaginate(
+            commentsAggregate,
+            options
+        )
+        if (!result || result.length === 0) {
+            return res
+                .status(200)
+                .json(new ApiResponse(200, {}, "No commments in this video!!"))
+        }
 
         return res.status(200)
-            .json(new ApiResponse(200, allComments, `Fetched Successfully  ${allComments.length} `))
+            .json(new ApiResponse(200, result, `Fetched Successfully  ${result.docs.length} `))
     } catch (error) {
         throw new ApiError(500, error.message || "Something went wrong!!");
     }
@@ -200,6 +220,217 @@ const getAllVideosComment = asyncHandler(async (req, res) => {
 })
 
 // 
+const addCommentToPost = asyncHandler(async (req, res) => {
+    const { comPostId } = req.params;
+    const { content } = req.body;
+    try {
+        if (!req.user?._id && !isValidObjectId(req.user?._id)) {
+            throw new ApiError(404, "UnAuthorized request!!");
+        }
+        if (!comPostId && !isValidObjectId(comPostId)) {
+            throw new ApiError(404, "No CommunityPost id found!!");
+        }
+        if (!content || content.trim() == "") {
+            throw new ApiError(401, "Content required!");
+        }
+        const findCommunityPost = await CommunityPost.findById(comPostId);
+        if (!findCommunityPost) {
+            throw new ApiError(400, "No Post ID found ");
+        }
+        const postComment = await Comment.create(
+            {
+                content: content.trim(),
+                commentedBy: req.user?._id,
+                communityPost: comPostId
+            }
+
+        );
+        if (!postComment) {
+            throw new ApiError(500, "Something went wrong!!");
+        }
+
+        return res.status(200)
+            .json(new ApiResponse(200, postComment, "Commented Successfully Community Post"))
+
+    } catch (error) {
+        throw new ApiError(500, error.message || "Something went wrong!!");
+    }
+});
+
+
+// 
+const updateCommentCommunityPost = asyncHandler(async (req, res) => {
+    const { content } = req.body;
+    const { commentId } = req.params;
+    try {
+        if (!req.user?._id && !isValidObjectId(req.user?._id)) {
+            throw new ApiError(404, "UnAuthorized request!!");
+        }
+        if (!commentId && !isValidObjectId(commentId)) {
+            throw new ApiError(404, "No CommunityPost id found!!");
+        }
+        if (!content || content.trim() == "") {
+            throw new ApiError(401, "Content required!");
+        }
+        const updateComment = await Comment.findOneAndUpdate(
+            {
+                $and: [
+                    { _id: commentId },
+                    { commentedBy: req.user?._id },
+                    {
+                        communityPost: {
+                            $exists: true,
+                        }
+                    }
+                ]
+            },
+            {
+                $set: {
+                    content: content.trim(),
+                }
+            }, {
+            new: true
+        }
+        );
+        if (!updateComment) {
+            throw new ApiError(400, "Cannot be updated");
+        }
+        return res.status(200)
+            .json(new ApiResponse(200, updateComment, "Updated Comment Post Successfully"))
+
+    } catch (error) {
+        throw new ApiError(500, error.message || "Something went wrong!!");
+    }
+});
+
+
+// 
+
+const deleteCommentCommunityPost = asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+    try {
+        if (!req.user?._id && !isValidObjectId(req.user?._id)) {
+            throw new ApiError(404, "UnAuthorized request!!");
+        }
+        if (!commentId && !isValidObjectId(commentId)) {
+            throw new ApiError(404, "No CommunityPost id found!!");
+        }
+        const deletedComment = await Comment.findOneAndDelete({
+            $and: [
+                { _id: commentId },
+                { commentedBy: req.user?._id },
+                {
+                    communityPost: {
+                        $exists: true
+                    }
+                }
+            ]
+        })
+        if (!deleteCommentVideo) {
+            throw new ApiError(500, "Something went wrong");
+        }
+        return res.status(200)
+            .json(new ApiResponse(200, {}, "Comment on  Community Post Deleted Succeesfully "))
+
+    } catch (error) {
+        throw new ApiError(500, error.message || "Something went wrong!!");
+    }
+});
+
+// 
+const getAllCommentsOnCommunityPost = asyncHandler(async (req, res) => {
+    const { page, limit, comPostId, sort } = req.query;
+    // const {  } = req.params;
+    try {
+        if (!req.user?._id && !isValidObjectId(req.user?._id)) {
+            throw new ApiError(404, "UnAuthorized request!!");
+        }
+        if (!comPostId && !isValidObjectId(comPostId)) {
+            throw new ApiError(404, "No video id found!!");
+        }
+        const commentsAggregate = Comment.aggregate([
+            {
+                $match: {
+                    communityPost: new mongoose.Types.ObjectId(comPostId),
+                },
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "commentedBy",
+                    foreignField: "_id",
+                    as: "owner",
+                },
+            },
+            {
+                $lookup: {
+                    from: "likes",
+                    localField: "_id",
+                    foreignField: "commentedBy",
+                    as: "likes",
+                },
+            },
+            {
+                $addFields: {
+                    likesCount: {
+                        $size: "$likes",
+                    },
+                    owner: {
+                        $first: "$owner",
+                    },
+                    isLiked: {
+                        $cond: {
+                            if: { $in: [req.user?._id, "$likes.likedBy"] },
+                            then: true,
+                            else: false
+                        }
+                    }
+                },
+            },
+            {
+                $project: {
+                    content: 1,
+                    createdAt: 1,
+                    updatedAt: 1,
+                    likesCount: 1,
+                    owner: {
+                        username: 1,
+                        fullName: 1,
+                        avatar: 1,
+                    },
+                    isLiked: 1
+                },
+            },
+        ]);
+        const options = {
+            page: parseInt(page, 10),
+            limit: parseInt(limit, 10),
+            sort: {
+                createdAt: parseInt(sort) || 1
+            }
+        };
+        if (!commentsAggregate) {
+            throw new ApiError(500, "Something went wrong!!");
+        }
+        const allComments = await Comment.aggregatePaginate(
+            commentsAggregate,
+            options
+        );
+
+
+        if (!allComments || allComments.length === 0) {
+            return res
+                .status(200)
+                .json(new ApiResponse(200, {}, "No commments in this video!!"))
+        }
+        return res.status(200)
+            .json(new ApiResponse(200, allComments, `Fetched Successfully  ${allComments.docs.length} `))
+
+    } catch (error) {
+        throw new ApiError(500, error.message || "Something went wrong!!");
+    }
+})
+
 
 
 
@@ -207,5 +438,9 @@ export {
     addCommentToVideo,
     updateCommentVideo,
     deleteCommentVideo,
-    getAllVideosComment
+    getAllVideosComment,
+    addCommentToPost,
+    updateCommentCommunityPost,
+    deleteCommentCommunityPost,
+    getAllCommentsOnCommunityPost
 }
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import {
+    addCommentToPost,
     addCommentToVideo,
+    deleteCommentCommunityPost,
     deleteCommentVideo,
+    getAllCommentsOnCommunityPost,
     getAllVideosComment,
+    updateCommentCommunityPost,
     updateCommentVideo,
 } from "../controllers/comment.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlware.js";
@@ -14,7 +18,13 @@ const router = Router()
 router.route("/addComment-vid/:videoId").post(verifyJWT, addCommentToVideo)
 router.route("/updateComment-vid/:commentId").patch(verifyJWT, updateCommentVideo)
 router.route("/deleteComment-vid/:commentId").delete(verifyJWT, deleteCommentVideo)
-router.route("/getAllComment-vid/:videoId").get(verifyJWT,getAllVideosComment)
+router.route("/getAllComment-vid").get(verifyJWT, getAllVideosComment)
 
 
+
+// for communitypost
+router.route("/addComment-com/:comPostId").post(verifyJWT, addCommentToPost)
+router.route("/updateComment-com/:commentId").patch(verifyJWT, updateCommentCommunityPost)
+router.route("/deleteComment-com/:commentId").delete(verifyJWT, deleteCommentCommunityPost)
+router.route("/getAllComment-com").get(verifyJWT, getAllCommentsOnCommunityPost)
 export default router;
